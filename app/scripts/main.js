7a83e2b226e8dc2bd735a71680a668cbfd540aec
kavinrajsi <we@sikavinraj.me>
2018-06-28 09:32:04 +0530

    home and living remotely page

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,16 @@
 console.log('\'Allo \'Allo!');
+$(document).ready(function() {
+
+   	var
+   	$body = $('body'),
+    $menuLeft = $('#navbar'),
+    $burgermenu = $('#nav-burgermenu'),
+    $burgermenuIcon = $('.buger-menu-icon');
+
+    $burgermenu.click(function() {
+        $burgermenuIcon.toggleClass('active');
+        $menuLeft.toggleClass('menu-drawer-open');
+        $body.toggleClass('menu-open');
+    });
+
+});
