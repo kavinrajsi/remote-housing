5525a6c8d17fc4eba17c95b64a16932b989d19d2
Robert Gillman <robert@equ.com.au>
2018-07-02 11:31:51 +0800

    Styled history page

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,16 +1,19 @@
-console.log('\'Allo \'Allo!');
 $(document).ready(function() {
 
-   	var
-   	$body = $('body'),
-    $menuLeft = $('#navbar'),
-    $burgermenu = $('#nav-burgermenu'),
-    $burgermenuIcon = $('.buger-menu-icon');
+   	var $body = $('body'),
+        $menuLeft = $('#navbar'),
+        $burgermenu = $('#nav-burgermenu'),
+        $burgermenuIcon = $('.buger-menu-icon');
 
     $burgermenu.click(function() {
         $burgermenuIcon.toggleClass('active');
         $menuLeft.toggleClass('menu-drawer-open');
         $body.toggleClass('menu-open');
+    });
+
+    $('.timeline-point').click(function() {
+        $('.timeline-point').removeClass('active');
+        $(this).addClass('active');
     });
 
 });
